a5efe47bdd0d690312a1bb370a655ff47791cd1f
Scott Jones <scott.jones9336@gmail.com>
2022-05-14 09:51:33 +0100

    Completed challenge 1

--- BadAndGoodStrings.java
+++ BadAndGoodStrings.java
@@ -1,6 +1,7 @@
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * BadAndGoodStrings
@@ -38,11 +39,12 @@ public class BadAndGoodStrings {
         }
         return true;
     }
-    public static List<Integer> sorted(List<Integer> current_splitting) {
+    public static int sorted(List<Integer> current_splitting) {
         // (a, b) -> (a+1, b-1)
         // OO -> EE, EE -> OO, OE -> EO, EO -> OE
         // OEO -> EOO -> EEE, OEEO -> EOEO -> EOOE -> EEEE
         // OEEEO -> EOEEO -> EOOOO -> EEEEE
+        int number_of_swaps = 0;
         for (int k = 0; k < 20; k++) {
             int currentLen = 0;
             // Even = 0, Odd = 1
@@ -65,9 +67,11 @@ public class BadAndGoodStrings {
                                 current_splitting.set(i-1, prevItem-1);
                                 current_splitting.set(i, item+1);
                             }
+                            number_of_swaps+=2;
                         } else if (prevType == 1 && currentLen > 0) {
                             current_splitting.set(i-2, current_splitting.get(i-2)+1);
-                            current_splitting.set(i-1, current_splitting.get(i-1)-1);   
+                            current_splitting.set(i-1, current_splitting.get(i-1)-1);  
+                            number_of_swaps+=2;
                         }
                         currentLen=1;
                     }
@@ -75,16 +79,55 @@ public class BadAndGoodStrings {
                 prevType = currentType;
             }
             if (isGood(current_splitting)) {
-                return current_splitting;
+                System.out.println(current_splitting);
+                return number_of_swaps;
             } else {
                 System.out.println(current_splitting);
             }
         }
-        return current_splitting;
+        return number_of_swaps;
+    }
+
+    public static int[] stringToArray(String test_case) {
+        int[] arr = new int[test_case.length()];
+        int i = 0;
+        for (char ch : test_case.toCharArray()) {
+            arr[i]=Character.getNumericValue(ch);
+            i+=1;
+        }
+        return arr;
+    }
+    public static int solve(int n, int[] test_case) {
+        int swaps = 0;
+        int group_length = 1;
+        for (int i = 1; i < n; i++) {
+            if (test_case[i-1] == test_case[i]) {
+                group_length+=1;
+            }
+            else if (group_length % 2 != 0) {
+                test_case[i]=test_case[i-1];
+                swaps+=1;
+                group_length=0;
+            }
+            else {
+                group_length=1;
+            }
+        }
+        return swaps;
     }
     public static void main(String[] args) {
-        String test_case = "000001110010100111110";
-        System.out.println(divider(test_case));
-        System.out.println(sorted(divider(test_case)));
+        Scanner input=new Scanner(System.in); 
+        int num_cases=input.nextInt(); 
+        for (int i = 0; i < num_cases-1; i++) {
+                int n =input.nextInt();
+                String test_case=input.next();
+                int[] test_arr = stringToArray(test_case);
+                System.out.println(solve(n, test_arr));
+        }
+        int n =input.nextInt();
+        String test_case=input.next();
+        int[] test_arr = stringToArray(test_case);
+        System.out.print(solve(n, test_arr));
+        input.close();
     }
 }
