78a4c4d18d87fd85ef5ed3b633c10345d80603b6
Scott Jones <scott.jones9336@gmail.com>
2022-05-15 16:49:28 +0100

    Create isLucky.c

---
+++ isLucky.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int LENGTH =6;
+typedef enum {true, false} bool;
+
+bool isGood(void) {
+    int el;
+    int sum;
+    for (int i=0; i<LENGTH; i++) {
+        scanf("%d", &el);
+        if (i<LENGTH/2) {
+            sum+=el;
+        }
+        else {
+            sum-=el;
+        }
+    }
+    return (sum==0);
+}
+int main(void) {
+    int test_cases;
+    scanf("%d", &test_cases);
+    for (int i=0; i<test_cases; i++) {
+        if (isGood()) {
+            printf("YES\n");
+        }
+        else {
+            printf("NO\n");
+        }
+    }
+}
