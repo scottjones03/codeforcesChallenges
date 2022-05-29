3c2297b793924a45c469f3ec27dbd0591b39902a
Scott Jones <scott.jones9336@gmail.com>
2022-05-29 11:29:51 +0100

    start of odd subarrays

---
+++ java/oddSubArrays.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * oddSubArrays
+*/
+public class oddSubArrays {
+    public static int[] take_slice(int[] arr, int start, int end) {
+        int[] sliced_arr = new int[end-start];
+        for (int i=start; i<end; i++) {
+            sliced_arr[i-start] = arr[i];
+        }
+        return sliced_arr;
+    }
+    
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in); 
+        int num_cases=input.nextInt(); 
+        for (int i = 0; i < num_cases; i++) {
+                int test_case_length =input.nextInt();
+                int[] test_case = new int[test_case_length];
+                for (int j =0; j< test_case_length; j++) {
+                    test_case[j] = input.nextInt();
+                }
+                int num_odd_arrs=0;
+                int current_split=0;
+                int current_combinations=0;
+                for (int j=0; j< test_case_length; j++) {
+                    for (int k=current_split; k< j; k++) {
+                        if (test_case[k] > test_case[j]) {
+                            current_combinations+=1;
+                        }
+                        if (current_combinations==2) {
+                            current_split=j;
+                            System.out.println(j);
+                        
+                            num_odd_arrs+=1;
+                            current_combinations=0;
+                        }
+                    }
+                } 
+           
+                
+                
+        }
+    }
+}
